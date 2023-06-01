22bb18e3f913d22bd64b8f9d2e09086c82013db2
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-01 22:48:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" xmlns:th="https://www.thymeleaf.com">
+<html lang="en">
 
 <head>
   <!-- Required meta tags -->
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css"
-    integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
   <link rel="stylesheet" href="static/css/style.css">
@@ -19,38 +18,12 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg bg-dark p-12" >
-    <div class="container-fluid">
-      <a class="navbar-brand text-white text-uppercase mr-6  pr-6" href="/"><strong> Monografias<span>_Store
-          </span></strong></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse ml-8" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 ms-2 mb-lg-0">
-          
-          <li class="nav-item">
-            <a class="nav-link active text-white m-2 " aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white m-2" href="/book_list">All Books</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white m-2" href="/my_book">My Book</a>
-          </li>
-          <li>
-            <a class="nav-link hidel m-2" href="bookRegister.html">Nova Monografia</a>
-          </li>  
-
-        </ul>
-        
-      </div>
-    </div>
+  <nav class="navbar navbar-expand-lg bg-dark p-12">
+    <?php include __DIR__ . '/div.php'; ?>
   </nav>
 
-  <div class="container text-center my-5">
-    <h1>Welcame to book store..</h1>
+  <div class="container text-center my-5 bg-white p-5 shadow-lg">
+    <h1>Bem Vindo ao Sistema de Monografias..</h1>
     <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
       Quo, sunt doloribus possimus eius necessitatibus dolores quibusdam
       accusantium reprehenderit. Suscipit corporis maiores ipsam facilis
@@ -64,12 +37,9 @@
 
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-  integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-  integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
 
 </html>
