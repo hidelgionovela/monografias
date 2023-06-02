da2a00bca98be2bb8b2ed0b4dfb891ce82da837d
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-02 23:02:52 +0200

    Create div2.php

---
+++ div2.php
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+         <a class="navbar-brand text-white text-uppercase mr-6  pr-6 h1" href="#"><strong> Monografias<span>_Store
+                           </span></strong></a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+         </button>
+        
+</div>
